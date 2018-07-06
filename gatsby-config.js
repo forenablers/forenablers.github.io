1ec2a3b477855207e572f9b638c8a0086e6e2d9e
zavolokas <sergey.zavoloka@gmail.com>
2018-07-06 14:16:49 +0200

    setup Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,5 +53,19 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography',
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-121878312-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    }
   ],
 }
