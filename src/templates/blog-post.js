e9b173ba8fddaae8970a90f2475dd43f30787dca
zavolokas <sergey.zavoloka@gmail.com>
2018-10-03 07:43:05 +0200

    install disqus comments

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import get from 'lodash/get'
+import { DiscussionEmbed } from 'disqus-react'
 
 import Bio from '../components/Bio'
 import { rhythm, scale } from '../utils/typography'
@@ -27,6 +28,11 @@ class BlogPostTemplate extends React.Component {
     const siteTitle = get(this.props, 'data.site.siteMetadata.title');
     const { previous, next } = this.props.pathContext;
     const blogPostUrl = post.frontmatter.postUrl;
+    const disqusShortname = "forenablers.disqus.com";
+    const disqusConfig = {
+      identifier: post.id,
+      title: post.frontmatter.title,
+    };
 
     return (
       <div>
@@ -95,6 +101,7 @@ class BlogPostTemplate extends React.Component {
             </li>
           )}
         </ul>
+        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
       </div>
     )
   }
